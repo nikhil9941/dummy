a26cc3c234589f961c189a8afac2da147157a17c
nikhil9941 <nikhiltarate03200@gmail.com>
2025-09-07 14:14:20 +0530

    final code

--- practice4.cpp
+++ practice4.cpp
@@ -4,13 +4,14 @@ int main(){
     string str;
     cout<<"Enter the string:";
     cin>>str;
-    int left=0,right=sizeof(str)-1;
+    int left=0,right=str.length()-1;
     while(left<right){
         if(str[left]!=str[right]){
-            cout<<"false";
+            cout<<"Is it palindrome:No";
             return 0;
         }
+        left++;right--;
     }
-    cout<<"true";
+    cout<<"Is it palindrome:Yes";
     return 0;
 }
