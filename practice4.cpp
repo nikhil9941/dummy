cb0964bf6513621fc3967040adde79e6fef53184
nikhil9941 <nikhiltarate03200@gmail.com>
2025-09-07 14:02:10 +0530

    code inserted

--- practice4.cpp
+++ practice4.cpp
@@ -1,5 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
+    string str;
+    cout<<"Enter the string:";
+    cin>>str;
+    int left=0,right=sizeof(str)-1;
+    while(left<right){
+        if(str[left]!=str[right]){
+            cout<<"false";
+            return 0;
+        }
+    }
+    cout<<"true";
     return 0;
 }
